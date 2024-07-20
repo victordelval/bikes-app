d91395ea731c595367ebc947964a07b95a7339c3
victordelval <victor.val.andres@gmail.com>
2024-07-20 20:15:55 +0200

    test: add basic data mock

---
+++ src/data/mocks.ts
@@ -0,0 +1,31 @@
+export const networks = [
+  {
+    id: 'network-1',
+    name: 'Network 1',
+    location: {
+      city: 'City 1',
+      country: 'Country 1',
+      countryCode: 'C1',
+      latitude: 41,
+      longitude: -2,
+    },
+    company: ['Company 1', 'Company 3'],
+  },
+  {
+    id: 'network-2',
+    name: 'Network 2',
+    location: {
+      city: 'City 2',
+      country: 'Country 2',
+      countryCode: 'C2',
+      latitude: 42,
+      longitude: -3,
+    },
+    company: ['Company 2'],
+  },
+];
+
+export const countries = [
+  { code: 'C1', name: 'Country 1' },
+  { code: 'C2', name: 'Country 2' },
+];
