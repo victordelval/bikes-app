ce57723526704d17cd7514786a8ba10c6fa43f09
victordelval <victor.val.andres@gmail.com>
2024-07-20 20:20:00 +0200

    chore: udpate data mock with domain types

--- src/data/mocks.ts
+++ src/data/mocks.ts
@@ -1,11 +1,16 @@
-export const networks = [
+import { Country } from "@/types/country";
+import { Network } from "@/types/network";
+
+export const networks: Network[] = [
   {
     id: 'network-1',
     name: 'Network 1',
     location: {
       city: 'City 1',
-      country: 'Country 1',
-      countryCode: 'C1',
+      country: {
+        name: 'Country 1',
+        code: 'C1',
+      },
       latitude: 41,
       longitude: -2,
     },
@@ -16,8 +21,10 @@ export const networks = [
     name: 'Network 2',
     location: {
       city: 'City 2',
-      country: 'Country 2',
-      countryCode: 'C2',
+      country: {
+        name: 'Country 2',
+        code: 'C2',
+      }, 
       latitude: 42,
       longitude: -3,
     },
@@ -25,7 +32,7 @@ export const networks = [
   },
 ];
 
-export const countries = [
+export const countries: Country[] = [
   { code: 'C1', name: 'Country 1' },
   { code: 'C2', name: 'Country 2' },
 ];
