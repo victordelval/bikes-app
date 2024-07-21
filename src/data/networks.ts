9b06372fbe76cff9ff2896232aa43bd188b4dfa0
victordelval <victor.val.andres@gmail.com>
2024-07-21 18:31:00 +0200

    feat: add fetch network details to data layer

--- src/data/networks.ts
+++ src/data/networks.ts
@@ -4,6 +4,11 @@ import { getCountries } from "./countries";
 
 const networksUrl = "http://api.citybik.es/v2/networks";
 
+
+/**
+ * All networks
+ */
+
 export type NetworkDTO = {
   id: string;
   name: string;
@@ -38,11 +43,77 @@ export async function getNetworks(): Promise<Network[]> {
   }));
 }
 
-export async function fetchNetworks(): Promise<NetworksDTO> {
+async function fetchNetworks(): Promise<NetworksDTO> {
   const res = await fetch(`${networksUrl}?fields=id,name,company,location`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch networks");
+  }
+
+  return await res.json();
+}
+
+/**
+ * Networks details: stations
+ */
+
+type StationsDTO = {
+  id: string;
+  name: string;
+  free_bikes: number;
+  empty_slots: number;
+  latitude: number;
+  longitude: number;
+  timestamp: string;
+};
+
+type NetworkStationsDTO = {
+  name: string;
+  stations: StationsDTO[];
+};
+
+type NetworkDetailsDTO = { network: NetworkStationsDTO };
+
+export type Station = {
+  id: string;
+  name: string;
+  freeBikes: number;
+  emptySlots: number;
+  latitude: number;
+  longitude: number;
+  // timestamp: string;
+};
+
+type NetworkDetails = {
+  name: string;
+  stations: Station[];
+};
+
+export async function getNetworkDetailsById(
+  id: string,
+): Promise<NetworkDetails> {
+  const networkDto = await fetchNetworkDetailsById(id);
+  const { network } = networkDto;
+
+  return {
+    ...network,
+    stations: network.stations.map((station) => ({
+      id: station.id,
+      name: station.name,
+      freeBikes: station.free_bikes,
+      emptySlots: station.empty_slots,
+      latitude: station.latitude,
+      longitude: station.longitude,
+      // timestamp: station.timestamp,
+    })),
+  };
+}
+
+async function fetchNetworkDetailsById(id: string): Promise<NetworkDetailsDTO> {
+  const res = await fetch(`${networksUrl}/${id}?fields=id,name,stations`);
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch network details with id: ${id}`);
   }
 
   return await res.json();
