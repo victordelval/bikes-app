9dcf66a843dd69450fd37494a6fb8a8545ff1890
victordelval <victor.val.andres@gmail.com>
2024-07-21 02:38:39 +0200

    test: search case to filter data

--- e2e/networks.spec.ts
+++ e2e/networks.spec.ts
@@ -11,6 +11,7 @@ test.beforeEach(async ({ page }) => {
 test.describe("Networks pages", () => {
   test("has titles", async ({ page }) => {
     await expect(page).toHaveTitle(/cyclemap app/i);
+    await expect(page.getByText(/cyclemap/i)).toBeVisible();
     await expect(page.locator("h1")).toContainText(/discover bike networks/i);
   });
 
@@ -22,5 +23,24 @@ test.describe("Networks pages", () => {
       .click();
     await expect(page).toHaveURL(baseURL + "/bicimad");
     await expect(page.getByRole("heading", { name: /bicimad/i })).toBeVisible();
+  });
+
+  test("should render filtered network list by search param with 1 network", async ({
+    page,
+  }) => {
+    await page.goto("/?search=bicimad");
+    await page.getByRole("link", { name: /details/i }).click();
+    await expect(page).toHaveURL(baseURL + "/bicimad");
+  });
+
+  test("should render filtered network list with 1 network by typing in searchbox", async ({
+    page,
+  }) => {
+    await page
+      .getByRole("searchbox", { name: /search network/i })
+      .fill("bicimad");
+    await page.waitForURL(baseURL + "/?search=bicimad");
+    await page.getByRole("link", { name: /details/i }).click();
+    await expect(page).toHaveURL(baseURL + "/bicimad");
   });
 });
